9fcc520cb0ece2e412fc470a86e4d59c6fc05b08
oiM-o <sugar_sugar2000@icloud.com>
2025-08-18 10:02:51 +0900

    ストアリリース時に自動でアプデ催促するように

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:medicine_reminder/ui/screen/home_screen.dart';
+import 'package:upgrader/upgrader.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,24 +13,31 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      localizationsDelegates: GlobalMaterialLocalizations.delegates,
-      supportedLocales: const [
-        Locale('ja', 'JP'),
-      ],
-      title: 'お薬リマインダー',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.teal),
-        useMaterial3: true,
-          fontFamily: 'Noto Sans JP'
-      ),
-      home: const HomeScreen(), // ← これで起動時にHomeScreenが出る
-      // ルーティングを使うならこんな感じで追加してもOK
-      // routes: {
-      //   '/home': (_) => const HomeScreen(),
-      //   '/settings': (_) => const SettingsScreen(),
-      // },
-      // initialRoute: '/home',
+          localizationsDelegates: GlobalMaterialLocalizations.delegates,
+          supportedLocales: const [
+            Locale('ja', 'JP'),
+          ],
+          title: 'お薬リマインダー',
+          debugShowCheckedModeBanner: false,
+          theme: ThemeData(
+            colorScheme: ColorScheme.fromSeed(seedColor: Colors.teal),
+            useMaterial3: true,
+              fontFamily: 'Noto Sans JP'
+          ),
+          home: UpgradeAlert(
+            upgrader: Upgrader(
+              debugDisplayAlways: false,
+              debugLogging: true,
+              countryCode: 'JP',
+            ),
+            child: const HomeScreen(),
+          // ルーティングを使うならこんな感じで追加してもOK
+          // routes: {
+          //   '/home': (_) => const HomeScreen(),
+          //   '/settings': (_) => const SettingsScreen(),
+          // },
+          // initialRoute: '/home',
+        ),
     );
   }
 }
